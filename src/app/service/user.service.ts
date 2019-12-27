c3409e24ec96d198f78dde3d318898fd4e032a48
Felipe Ferraz <Felipe_net_18@hotmail.com>
2019-12-27 19:41:17 +0000

    Fix errors

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -10,19 +10,19 @@ export class UserService {
   }
 
   create(vehicle) {
-      return this.http.post(this.vehicleEndpoint, vehicle, this.addHeaders());
+    // return this.http.post(this.vehicleEndpoint, vehicle, this.addHeaders());
   }
 
   addHeaders() {
-      const access_token = localStorage.getItem('access_token');
-      return { headers: new HttpHeaders().set('Authorization', `Bearer
+    const access_token = localStorage.getItem('access_token');
+    return { headers: new HttpHeaders().set('Authorization', `Bearer
   ${access_token}`) };
   }
 
   getCurrentUser() {
     return new Promise((resolve, reject) => {
       return this.http.get('http://localhost:3000/api/v1/auth/me').toPromise().then(response => {
-        resolve(response.json());
+        resolve(response);
       }).catch(() => reject());
     });
   }

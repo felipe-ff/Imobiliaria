e8db678e57c982b7214d82e7684364bf26409707
Felipe <felipe.ferraz18@gmail.com>
2019-12-31 10:12:43 -0300

    Fix add house

--- src/app/service/cdr.service.ts
+++ src/app/service/cdr.service.ts
@@ -16,7 +16,7 @@ export class CdrService {
     const port = isDevMode() ? '8081' : '';
     const url = isDevMode() ? 'http://localhost:' : 'https://imobiliaria-db.appspot.com';
     this.baseUrlApi = url + port + '/api/books';
-    this.baseUrl = url + port;
+    this.baseUrl = url + port + '/books';
   }
 
   getBooks(filters) {
@@ -41,58 +41,10 @@ export class CdrService {
   }
 
   addBook(formData) {
-    return this.http.post(this.baseUrlApi + '/add', formData);
+    return this.http.post(this.baseUrl + '/add', formData);
   }
 
   updateBook(id, formData) {
     return this.http.put<any>(this.baseUrlApi + '/' + id, formData);
-  }
-
-  getCdrsWithPaging(filter: any, page: string, size: string) {
-    /*.set('sort', 'duration').set('duration.dir', 'desc')*/
-    const params = this.setDefaultFilters(filter, {name: 'subscriber', value: filter.subscriber}, page, size);
-    return this.http.get<any>(this.baseUrl, {params});
-  }
-
-  getSubscribers(entityId: string, filter: any, page: string, size: string) {
-    const params = this.setDefaultFilters(filter, {name: 'entityId', value: entityId}, page, size);
-    return this.http.get<any>(this.baseUrl + '/entity', {params});
-  }
-
-  getSubscribersAll(entityId: string, filter: any) {
-    const params = this.setDefaultFilters(filter, {name: 'entityId', value: entityId}, null, null);
-    return this.http.get<any>(this.baseUrl + '/entity/all', {params});
-  }
-
-  getDestinationChart(entityId: string, filter: any) {
-    const params = this.setDefaultFilters(filter, {name: 'entityId', value: entityId}, null, null);
-    return this.http.get<any>(this.baseUrl + '/chart/destination', {params});
-  }
-
-  getTotalValueChart(entityId: string, filter: any) {
-    const params = this.setDefaultFilters(filter, {name: 'entityId', value: entityId}, null, null);
-    return this.http.get<any>(this.baseUrl + '/chart/totalValue', {params});
-  }
-
-  getCdrChart(entityId: string, filter: any) {
-    const params = this.setDefaultFilters(filter, {name: 'entityId', value: entityId}, null, null);
-    return this.http.get<any>(this.baseUrl + '/chart/cdrs', {params});
-  }
-
-  setDefaultFilters(filter, objId, page, size) {
-    const params = new HttpParams()
-      .set('page', page || '')
-      .set('size', size || '')
-      .set(objId.name, objId.value)
-      .set('startDatetime', filter.startDatetime || '')
-      .set('endDatetime', filter.endDatetime || '')
-      .set('minDuration', filter.minDuration || '')
-      .set('maxDuration', filter.maxDuration || '')
-      .set('minValue', filter.minValue || '')
-      .set('maxValue', filter.maxValue || '')
-      .set('fromNumber', filter.fromNumber || '')
-      .set('toNumber', filter.toNumber || '')
-      .set('callQueryType', filter.callQueryType.code || '');
-    return params;
   }
 }
